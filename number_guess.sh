7b1cd1d6ea12d556ff449279c5ebd1eba5e7fcc7
Yonatan Margalit <tunaboy1999@gmail.com>
2026-05-23 12:13:37 -0400

    feat: variable defining and welcome messages

--- number_guess.sh
+++ number_guess.sh
@@ -3,3 +3,15 @@ PSQL="psql --username=freecodecamp --dbname=number_guess -t --no-align -c"
 
 echo "Enter your username:"
 read USERNAME
+
+USER_EXISTS=$($PSQL "SELECT username FROM users WHERE username='$USERNAME'")
+GAMES_PLAYED=$($PSQL "SELECT COUNT(*) FROM users INNER JOIN games USING(user_id) WHERE username='$USERNAME'")
+BEST_GAME=$($PSQL "SELECT MIN(number_guesses) FROM users INNER JOIN games USING(user_id) WHERE username='$USERNAME'")
+
+if [[ -z $USER_EXISTS ]]
+then
+  NEW_USER=$($PSQL "INSERT INTO users(username) VALUES ('$USERNAME')") > /dev/null
+  echo "Welcome, $USERNAME! It looks like this is your first time here."
+else
+  echo "Welcome back, $USERNAME! You have played $GAMES_PLAYED games, and your best game took $BEST_GAME guesses."
+fi
